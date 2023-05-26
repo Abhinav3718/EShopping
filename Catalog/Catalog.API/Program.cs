872828cd871e8fb3b47ebd6e1fbfb5c14f103aa8
Abhinav Oruganti <orugantiabhinav5@gmail.com>
2023-05-26 19:13:42 +0530

    Add project files.

---
+++ Catalog/Catalog.API/Program.cs
@@ -0,0 +1,14 @@
+using Catalog.API;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        return Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(builder => builder.UseStartup<Startup>());
+    }
+}
---
+++ Catalog/Catalog.API/Startup.cs
@@ -0,0 +1,36 @@
+﻿namespace Catalog.API
+{
+    public class Startup
+    {
+        public IConfiguration Configuration;
+
+        public Startup(IConfiguration _configuration)
+        {
+            Configuration = _configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if(env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseAuthentication();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+        }
+    }
+}
