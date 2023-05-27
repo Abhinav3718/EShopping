d970935b3f9f75b163315e2977b6ca3a1cf46bbf
Abhinav Oruganti <orugantiabhinav5@gmail.com>
2023-05-27 17:30:44 +0530

    Interface Repositories basic added

---
+++ Catalog/Catalog.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Core.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllProductsAsync();
+        Task<Product> GetProductAsync(string id);
+        Task<IEnumerable<Product>> GetProductsByNameAsync(string productName);
+        Task<IEnumerable<Product>> GetProductsByBrandAsync(string brandName);
+        Task<Product> CreateProductAsync(Product product);
+        Task<bool> DeleteProductAsync(string id);
+        Task<bool> UpdateProductAsync(Product product);
+    }
+}
